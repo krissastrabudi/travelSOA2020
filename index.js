4341cf8ace532fa154456c04bdb211c597e774fa
Sastrabudi <sastrabudi@gmail.com>
2020-06-08 13:55:39 +0700

    configfinal

--- index.js
+++ index.js
@@ -20,4 +20,4 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send("Hello world"))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
